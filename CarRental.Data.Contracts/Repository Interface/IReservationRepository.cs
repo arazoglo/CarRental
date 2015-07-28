03a001578da448aaba8866ba043ff5d4815d006c
arazoglo <arazoglo@hotmail.com>
2015-07-28 17:37:35 +0100

    Adding left over from the last commit

---
+++ CarRental.Data.Contracts/Repository Interface/IReservationRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarRental.Business.Entities;
+using Core.Common.Contract;
+
+namespace CarRental.Data.Contracts
+{
+    public interface IReservationRepository : IDataRepository<Reservation>
+    {
+        IEnumerable<Reservation> GetReservationByPickupDate(DateTime pickupDate);
+        IEnumerable<CustomerReservationInfo> GetCurrentCustomerReservationInfo();
+        IEnumerable<CustomerReservationInfo> GetCustomerOpenReservationInfo(int accountId);
+    }
+}
