be4ef8ecf2284d9121ed8c15ff1a0293841aad42
arazoglo <arazoglo@hotmail.com>
2015-08-20 15:42:03 +0100

    Completing data repository factory, data access layer and some test methods

--- CarRental.Data.Test/DataLayerTests.cs
+++ CarRental.Data.Test/DataLayerTests.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Core.Common.Core;
 using CarRental.Business.Bootstrapper;
-using System.ComponentModel.Composition;
+using System.ComponentModel.Composition; 
 using CarRental.Data.Contracts;
+using CarRental.Data;
 using System.Collections;
 using CarRental.Business.Entities;
 using Moq;
+using Core.Common.Contract;
 
 namespace CarRental.Data.Test
 {
@@ -30,6 +32,58 @@ namespace CarRental.Data.Test
 
             //Assert.IsTrue(cars != null);
         }
+
+        [TestMethod]
+        public void test_repository_factory_usage()
+        {
+            RepositoryFactoryTestClass factoryTest = new RepositoryFactoryTestClass();
+
+            IEnumerable<Car> cars = factoryTest.GetCars();
+
+            //Assert.IsTrue(cars != null);
+        }
+
+        [TestMethod]
+        public void test_factory_mocking1()
+        {
+            List<Car> cars = new List<Car>()
+            {
+                new Car() {CarId = 1, Description="Mustang"},
+                new Car() {CarId = 2, Description="Corvette"}
+            };
+
+            Mock<IDataRepositoryFactory> mockDataRepository = new Mock<IDataRepositoryFactory>();
+            mockDataRepository.Setup(obj => obj.GetDataRepository<ICarRepository>().Get()).Returns(cars);
+
+            RepositoryFactoryTestClass factoryTest = new RepositoryFactoryTestClass(mockDataRepository.Object);
+
+            IEnumerable<Car> ret = factoryTest.GetCars();
+
+            Assert.IsTrue(ret == cars);
+        }
+
+        [TestMethod]
+        public void test_factory_mocking2()
+        {
+            List<Car> cars = new List<Car>()
+            {
+                new Car() {CarId=1,Description="Mustang"},
+                new Car() {CarId = 2,Description= "Merc"}
+            };
+
+            Mock<ICarRepository> mockCarRepository = new Mock<ICarRepository>();
+            mockCarRepository.Setup(obj => obj.Get()).Returns(cars);
+
+            Mock<IDataRepositoryFactory> mockDataRepository = new Mock<IDataRepositoryFactory>();
+            mockDataRepository.Setup(obj=>obj.GetDataRepository<ICarRepository>()).Returns(mockCarRepository.Object);
+
+            RepositoryFactoryTestClass factoryTest = new RepositoryFactoryTestClass(mockDataRepository.Object);
+
+            IEnumerable<Car> ret = factoryTest.GetCars();
+
+            Assert.IsTrue(ret == cars);
+        }
+
 
         [TestMethod]
         public void test_repository_mocking()
@@ -70,6 +124,32 @@ namespace CarRental.Data.Test
         public IEnumerable<Car> GetCars()
         {
             IEnumerable<Car> cars = _CarRepository.Get();
+
+            return cars;
+        }
+    }
+
+    public class RepositoryFactoryTestClass
+    {
+        public RepositoryFactoryTestClass()
+        {
+            ObjectBase.Container.SatisfyImportsOnce(this);
+        }
+
+        public RepositoryFactoryTestClass(IDataRepositoryFactory dataRepositoryFactory)
+        {
+            _DataRepositoryFactory = dataRepositoryFactory;
+        }
+
+        [Import]
+        IDataRepositoryFactory _DataRepositoryFactory;      
+
+        public IEnumerable<Car> GetCars()
+        {
+            ICarRepository carRepository
+                = _DataRepositoryFactory.GetDataRepository<ICarRepository>();
+
+            IEnumerable<Car> cars = carRepository.Get();
 
             return cars;
         }
---
+++ CarRental.Data/DataRepositoryFactory.cs
@@ -0,0 +1,24 @@
+﻿using Core.Common.Contract;
+using Core.Common.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Common.Contract;
+using Core.Common.Core;
+using System.ComponentModel.Composition;
+
+namespace CarRental.Data.Contracts
+{
+    [Export(typeof(IDataRepositoryFactory))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class DataRepositoryFactory : IDataRepositoryFactory
+    {
+
+        T IDataRepositoryFactory.GetDataRepository<T>()
+        {                                                   
+            return ObjectBase.Container.GetExportedValue<T>();
+        }
+    }
+}
